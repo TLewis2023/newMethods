168f9e82dbfac2aeebd0bb976466d705aff10fc6
TLewis2023 <127235673+TLewis2023@users.noreply.github.com>
2023-03-24 15:28:10 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,97 @@
+package org.example;
+
+public class Main {
+    public Main() {
+
+    }
+
+// public static String main(String[] args) {
+
+
+    //String fullName = "TahJae Lewis";
+
+
+    // System.out.println(fullName);
+
+     //Q3
+       /* public static char buyCoffee(){
+            return "$1";
+        }
+
+    public static void main(string[] args){
+
+        System.out.println(buyCoffee());
+    }
+
+
+
+    }*/
+
+    /*Question 4:
+    create a static method named myHobbies.
+    The method accepts only one parameter which is an array of your hobbies.
+    The method does not return any values. It should just print out your hobbies when invoked
+    Call or invoke your method.*/
+    //public static void hobbies(){
+
+}
+
+    /*Question 5:
+             create a static method named checkWeather.
+            The method accepts two parameters String weatherCondition and an int temperature.
+            The method will RETURN one of the values below based on the weather condition
+
+            - if weatherCondition is hot and temperature is greater than 80 - " Wear shorts "
+            - if weatherCondition is cool and temperature is greater than 50  - " Wear wind breakers "
+            - if weatherCondition is cold and temperature is below 50 - " Wear a jacket "
+            - if weatherCondition is anything else -  " No such weather condition "
+
+            Call or invoke your method.*/
+  /*   double weatherCondition;
+    if(weatherCondition >= 80){
+        System.out.println("wear shorts");
+}   else if(weatherCondition >= 50)){
+        System.out.println("wear a jacket");
+    }else if(weatherCondition <= 50)){
+        System.out.println("wear wind breaker");
+
+
+}
+
+
+    /*Question 6:
+    create a static method named areaOfTriangle.
+    The method accepts two parameters (base and height)
+    The method should just return the computed area of the triangle
+    Call or invoke your method.*/
+   /* public static void areaOfTriangle(){
+    System.out.println("base and height");
+}
+    public static String areaOfTriangle(){
+
+    return "area";
+
+
+}
+/*Question 7:
+         Randomly generate a number between 1 and 6
+         Create a switch statement with different cases to print out the random number generated
+         eg: case 1 -  you rolled a one ..etc*/
+
+  /*  int i = 1;
+    for {
+        System.out.println("you rolled a: " + i);
+        i++;
+        while (i < 6);
+    }
+}
+
+
+   */
+
+ /*Question 8:
+         Using the Scanner class. Prompt a user for their name
+        - If their name is Aliyah or Hakeem. Print out "Hello Aliyah" or "Hello Hakeem"
+        - If the user's name is not Aliyah or Hakeem then keep asking for their name
+        - Note: a user may type the letters of their name in uppercase or lowercase
+        eg: aLiYah, aLIYah or HakEEm, hAkEeM. Your code should work regardless
